e4859b27370a954f51e4d854faf03e41f14a1eaa
RAFIQUL595 <rafikulislam100000@gmail.com>
2024-12-22 19:53:08 +0600

    login and logout done

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import auth from "../../firebase/firebase.info";
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 export const AuthContext = createContext(null);
 
 const googleProvider = new GoogleAuthProvider();
@@ -15,17 +15,46 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  // Login With Email and Password
+  const handelLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
   // Register With Google
   const handelGoogleRegister = () => {
     setLoading(true);
     return signInWithPopup(auth, googleProvider);
   };
 
+  // Sign Out User
+  const handelSignOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    // Firebase auth state listener
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    // Cleanup the auth listener when component is unmounted
+    return () => unsubscribe();
+  }, []);
+
   const authInfo = {
     user,
     loading,
     handelRegister,
     handelGoogleRegister,
+    handelLogin,
+    handelSignOut,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,90 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import SocialLogin from '../shared/SocialLogin/SocialLogin';
+import { Helmet } from 'react-helmet';
+import lottieLogin from '../../assets/lottiefiles/login.json';
+import Lottie from 'lottie-react';
+import { AuthContext } from '../../context/AuthContext/AuthProvider';
+import Swal from 'sweetalert2';
+import toast from 'react-hot-toast';
 
 const Login = () => {
+    const { handelLogin } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const handleSubmitLogin = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        // Get the currently signed-in user
+        handelLogin(email, password)
+            .then((result) => {
+                Swal.fire({
+                    title: "Login Successful!",
+                    text: `Your account has been created successfully. Welcome ${result?.user?.displayName}`,
+                    icon: "success",
+                });
+                form.reset();
+                navigate("/");
+            })
+            .catch((error) => {
+                toast.error("Login failed. Please check your credentials.");
+            });
+    }
     return (
-        <div>
-            
+        <div className="hero-content my-10 flex-col gap-5 lg:flex-row-reverse">
+            <Helmet>
+                <title>Login | Car Rental</title>
+            </Helmet>
+            <div className="w-96 text-center lg:text-left">
+                <Lottie animationData={lottieLogin} />
+            </div>
+            <div className="card bg-base-100 w-full border-2 max-w-sm shrink-0 shadow-2xl">
+                <form onSubmit={handleSubmitLogin} className="p-8">
+                    <h1 className="text-2xl text-center font-bold">Login now!</h1>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">
+                                Email <span className="text-red-500">*</span>
+                            </span>
+                        </label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Enter your email"
+                            className="input input-bordered"
+                            required
+                        />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">
+                                Password <span className="text-red-500">*</span>
+                            </span>
+                        </label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Enter your password"
+                            className="input input-bordered"
+                            required
+                        />
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary text-lg">Login</button>
+                    </div>
+                </form>
+                <SocialLogin />
+                <div className="my-8 text-center">
+                    <p>
+                        Don't have an account?{" "}
+                        <Link to="/register" className="text-blue-500">
+                            Register
+                        </Link>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
