4a0f2542b7efd15f0b7c06ff2f8794a1b2469631
RAFIQUL595 <rafikulislam100000@gmail.com>
2024-12-22 16:40:03 +0600

    registration with email done

---
+++ src/context/AuthContext/AuthProvider.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useState } from "react";
+import auth from "../../firebase/firebase.info";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Register With Email and Password
+  const handelRegister = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = {
+    user,
+    loading,
+    handelRegister,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
---
+++ src/pages/Registration/Registration.jsx
@@ -0,0 +1,117 @@
+import React, { useContext } from "react";
+import lottieRegister from "../../assets/lottiefiles/register.json";
+import Lottie from "lottie-react";
+import { Helmet } from "react-helmet";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext/AuthProvider";
+import toast from "react-hot-toast";
+
+const Registration = () => {
+  const { handelRegister } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const photo = form.photo.value;
+    const password = form.password.value;
+    handelRegister(email, password)
+      .then((result) => {
+        if (result) {
+          toast.success("Registered successfully");
+          navigate("/login");
+        }
+        form.reset();
+      })
+      .catch((error) => {
+        toast.error(
+          "User already exists. Please log in or use a different email."
+        );
+      });
+  };
+  return (
+    <div className="hero-content my-10 flex-col gap-5 lg:flex-row-reverse">
+      <Helmet>
+        <title>Register | Car Rental</title>
+      </Helmet>
+      <div className="text-center lg:text-left">
+        <Lottie animationData={lottieRegister} />
+      </div>
+      <div className="card bg-base-100 w-full border-2 max-w-sm shrink-0 shadow-2xl">
+        <form onSubmit={handleSubmit} className="card-body">
+          <h1 className="text-2xl text-center font-bold">Register now!</h1>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">
+                Name <span className="text-red-500">*</span>
+              </span>
+            </label>
+            <input
+              type="name"
+              name="name"
+              placeholder="Enter your name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">
+                Email <span className="text-red-500">*</span>
+              </span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">
+                Photo URL <span className="text-red-500">*</span>
+              </span>
+            </label>
+            <input
+              type="text"
+              name="photo"
+              placeholder="Enter your photo URL"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">
+                Password <span className="text-red-500">*</span>
+              </span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Register</button>
+          </div>
+        </form>
+        <div className="card-body text-center">
+          <p>
+            Already have an account?{" "}
+            <Link to="/login" className="text-blue-500">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Registration;
